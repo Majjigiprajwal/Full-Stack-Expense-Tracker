dda7fda4ac5aab00d5a5bfe56de7b6a540a2999b
Prajwal G Majjigi <prajwal1majjigi@gmail.com>
2024-01-04 21:33:41 +0530

    configured morgan for logging the access,helmet for response headers .env file.

--- server/util/database.js
+++ server/util/database.js
@@ -1,9 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('expense-tracker','root','fullstack@123',{
-    dialect:'mysql',
-    host:'localhost'
+const sequelize = new Sequelize(process.env.DATABASE_NAME,process.env.DATABASE_USER_NAME,process.env.DATABASE_PASSWORD,{
+    dialect:process.env.DATABASE_DIALECT,
+    host:process.env.DATABASE_HOST
 })
-
 
 module.exports = sequelize;
