67a13e93a0595b14b65b5ca1087bc7e0583b7b09
Prajwal G Majjigi <prajwal1majjigi@gmail.com>
2024-01-11 02:36:41 +0530

    changed the code due to deployment error

--- client/src/components/Transactions/Transactions.js
+++ client/src/components/Transactions/Transactions.js
@@ -18,7 +18,7 @@ const Transactions = ({data,handleDelete,openModal,update}) => {
        <div className=" m-2 w-3/5 mb:w-8/12  no-scrollbar overflow-hidden ">
         <div className="flex">
           <div className="flex text-yellow-400 bg-slate-800 rounded-xl w-fit px-2 py-1 ">
-            {/* <svg
+            <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
               viewBox="0 0 20 20"
@@ -29,7 +29,7 @@ const Transactions = ({data,handleDelete,openModal,update}) => {
                 d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
                 clipRule="evenodd"
               />
-            </svg> */}
+            </svg>
             <p className="mb:text-xs  ml-1 font-medium text-sm">
              {date}
             </p>
